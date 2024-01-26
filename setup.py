61370a34546cb621900a25ab3dd3895c014b30da
Asaf Zorea <zoreasaf@gmail.com>
2024-01-26 11:55:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="MIT",
-    url="https://github.com/noisereduce/TensorFlowGating",
+    url="https://github.com/noisereduce/TFSpectralGating",
     packages=setuptools.find_packages(
         exclude=["tests", "*.tests", "*.tests.*", "tests.*", "tests.*"]
     ),
